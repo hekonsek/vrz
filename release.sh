48472caaab468e8d83d1a932d84ee2650fef4e09
Henryk Konsek <hekonsek@gmail.com>
2025-04-24 13:10:46 +0200

    Added git support.

--- release.sh
+++ release.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 set -e
-poetry version minor
-NEW_VERSION=$(poetry version -s)
+
+poetry run vrz minor
+
 git add pyproject.toml
+NEW_VERSION=$(poetry run vrz latest)
 git commit -m "Released version v$NEW_VERSION"
-git tag "v$NEW_VERSION"
-git push origin main --tags
+git push
 
 poetry publish --build
