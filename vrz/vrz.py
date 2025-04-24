92a40644ad43f078248e3d032176feee910ce51c
Henryk Konsek <hekonsek@gmail.com>
2025-04-24 12:53:32 +0200

    Added typer.

--- vrz/vrz.py
+++ vrz/vrz.py
@@ -1,5 +1,19 @@
+from typer import Typer
+
 def main():
-    print("Hello, VRZ!")
+    app = Typer(no_args_is_help=True)
+
+    @app.command()
+    def minor():
+        # poetry version minor
+        print(f"Hello!")
+
+    @app.command()
+    def latest():
+        # poetry version -s
+        print(f"Hello!")
+
+    app()
 
 if __name__ == "__main__":
     main()
