50ae80fadc36b8627c2076b92b852d8d266b4f47
Henryk Konsek <hekonsek@gmail.com>
2025-04-25 08:49:32 +0200

    Extracted CLI logic.

--- vrz/core.py
+++ vrz/core.py
@@ -1,19 +1,17 @@
 import shlex
 import subprocess
-from typing import Optional
-from typer import Typer
-import typer
 import requests as request
+
 
 class Poetry:
     def version_bump_minor(self):
-        output = subprocess.run(
+        subprocess.run(
             shlex.split("poetry version minor"),
             check=True,
             capture_output=True,
             text=True,
         )
-    
+
     def version_read(self):
         output = subprocess.run(
             shlex.split("poetry version -s"),
@@ -50,6 +48,7 @@ class Poetry:
         )
         return output.stdout.split()[0].strip()
 
+
 class Git:
     def is_git_repo(self):
         try:
@@ -62,7 +61,7 @@ class Git:
             return True
         except subprocess.CalledProcessError:
             return False
-        
+
     def create_tag(self, tag_name):
         subprocess.run(
             shlex.split(f"git tag {tag_name}"),
@@ -103,6 +102,7 @@ class Git:
             text=True,
         )
 
+
 class VersionSubstitution:
     def replace_version(self, file_path: str, old_version: str, new_version: str):
         with open(file_path, "r") as file:
@@ -112,50 +112,3 @@ class VersionSubstitution:
 
         with open(file_path, "w") as file:
             file.write(new_content)
-
-def main():
-    poetry = Poetry()
-    git = Git()
-    version_substitution = VersionSubstitution()
-    app = Typer(no_args_is_help=True)
-
-    @app.command()
-    def minor(update_file: Optional[list[str]] = typer.Option(default=None)):
-        old_version = poetry.version_read()
-        poetry.version_bump_minor()
-        typer.echo(f"Version bumped to {poetry.version_read()}.")
-        if git.is_git_repo():
-            tag_name = f"v{poetry.version_read()}"
-
-            git.add("pyproject.toml")
-            git.commit(f"Released {tag_name}.")
-            git.push()
-            typer.echo("Pushed updated pyproject.toml.")
-
-            git.create_tag(tag_name)
-            git.push_tag(tag_name)
-            typer.echo(f"Git tag {tag_name} created and pushed.")
-
-        if update_file:
-            for file in update_file:
-                typer.echo(f"Updating version in file: {file}")
-                version_substitution.replace_version(file, old_version, poetry.version_read())
-                git.add(file)
-            git.commit(f"Updated version to {poetry.version_read()}.")
-            git.push()
-            typer.echo("Pushed updated files.")
-
-        if poetry.is_current_project_published():
-            typer.echo("Publishing package to PyPI.")
-            poetry.publish()
-            typer.echo("Publishing to PyPI done.")
-
-
-    @app.command()
-    def latest():
-        typer.echo(poetry.version_read())
-
-    app()
-
-if __name__ == "__main__":
-    main()
